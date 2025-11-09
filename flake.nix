a73fa4c33e65d6c476e821bb686516c3dca2ee3d
NACAMURA Mitsuhiro <m15@m15a.dev>
2025-11-10 00:55:58 +0900

    chore: use self for formatting

--- flake.nix
+++ flake.nix
@@ -8,12 +8,19 @@
   outputs =
     { flakelight, treefmt-nix, ... }:
     flakelight ./. {
-      imports = [ flakelight.flakelightModules.extendFlakelight ];
+      imports = [
+        flakelight.flakelightModules.extendFlakelight
+        ./flakelight-treefmt.nix
+      ];
       flakelightModule =
         { lib, ... }:
         {
           imports = [ ./flakelight-treefmt.nix ];
           inputs.treefmt-nix = lib.mkDefault treefmt-nix;
         };
+      treefmtConfig = {
+        programs.nixfmt.enable = true;
+        programs.mdformat.enable = true;
+      };
     };
 }
